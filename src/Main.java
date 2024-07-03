bfa19ebd6e288598e6be2da539ea673f664ebd6b
Lukas Urba <lukas.urba@gmail.com>
2024-07-03 18:38:50 +0100

    Tried to calculate the numbers with largest differences when adjacent but didn't work

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,31 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(minDifference(new int[]{3,100,20}));
+        System.out.println(minDifference(new int[]{6,6,0,1,1,4,6}));
     }
     public static int minDifference(int[] nums) {
+        int counter1 = 0, counter2 = nums.length;
         Arrays.sort(nums);
         if (nums.length<3) {
             nums[nums.length-1] = nums[0];
         } else {
-            for (int i = 1;i <=3; i++) {
-                nums[nums.length-i] = nums[0];
+            for (int i = 0; i <3; i++) {
+                int highestDiff = 0;
+//                int difference1 = Math.abs(nums[counter1]-nums[counter1+1]), difference2 = Math.abs(nums[counter2-1]-nums[counter2-2]);
+//                if (difference1>=difference2) {
+//                    nums[counter1] = nums[counter1+1];
+//                    counter1++;
+//
+//                } else {
+//                    nums[counter2-1] = nums[counter2-2];
+//                    counter2--;
+//
+//                }
             }
         }
 //        Sort the array again to see the difference
         Arrays.sort(nums);
+        System.out.println(Arrays.toString(nums));
         return nums[nums.length-1]-nums[0];
     }
 }
