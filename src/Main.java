998af1bd030a6ce28ca6e2f70fc7de3fd159ccdc
Lukas Urba <lukas.urba@gmail.com>
2024-07-03 22:53:17 +0100

    Used method using minimum to compare the difference rather than actually manipulating the array

--- src/Main.java
+++ src/Main.java
@@ -5,48 +5,18 @@ public class Main {
         System.out.println(minDifference(new int[]{6,6,0,1,1,4,6}));
     }
     public static int minDifference(int[] nums) {
-        int counter1 = 0, counter2 = nums.length;
-        Arrays.sort(nums);
-        if (nums.length<3) {
-            nums[nums.length-1] = nums[0];
-        } else {
-            for (int i = 0; i <3; i++) {
-                int highestDiff = 0, diffIndex1 = 0, diffIndex2 = 1;
-                for (int j = 0; j < counter2-1; j++) {
-                    if(nums[j+1]-nums[j]>highestDiff) {
-//                        System.out.println(nums[j] + " " + nums[j+1]);
-//                        System.out.println(nums[j+1]-nums[j]);
-//                        System.out.println(highestDiff);
-//                        System.out.println();
-                        highestDiff = nums[j+1]-nums[j];
-                        diffIndex1 = nums[j];
-                        diffIndex2 = nums[j+1];
-                    }
-                }
-                        System.out.println(highestDiff);
-                        System.out.println();
-                if (nums.length-1-diffIndex2< diffIndex1-nums[0]) {
-                    nums[diffIndex1] = nums[diffIndex2];
-                } else {
-                    nums[diffIndex2] = nums[diffIndex1];
-
-                }
-
-//                int difference1 = Math.abs(nums[counter1]-nums[counter1+1]), difference2 = Math.abs(nums[counter2-1]-nums[counter2-2]);
-//                if (difference1>=difference2) {
-//                    nums[counter1] = nums[counter1+1];
-//                    counter1++;
-//
-//                } else {
-//                    nums[counter2-1] = nums[counter2-2];
-//                    counter2--;
-//
-//                }
-            }
+//        For any arrays shorter than 5, with 3 moves all numbers will be the same
+        if (nums.length<=4) {
+            return 0;
         }
-//        Sort the array again to see the difference
+//        Sort moved down so memory is not wasted if array too short on sorting
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        return nums[nums.length-1]-nums[0];
+        int diff = nums[nums.length-1] - nums[0];
+        for (int i = 0; i <=3; i++) {
+//            Compares the current difference of the whole array, with the difference between numbers in array with 3 elements removed from either side
+            diff = Math.min(diff, nums[nums.length-(3-i)-1]-nums[i]);
+        }
+
+        return diff;
     }
 }
